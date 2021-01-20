c1e9bd77c50ba0319ab2caabb472d3097138a192
Amalia Bryant <amaliacbryant@gmail.com>
2021-01-20 02:22:21 -0800

    the bot can move

--- scripts/script.js
+++ scripts/script.js
@@ -5,9 +5,7 @@ const gameInfoEl = document.querySelector(".gameInfo");
 const gameStatsEl = document.querySelector(".gameStats");
 const gameControlsEl = document.querySelector(".gameControls");
 
-let boardSize = 8;
-
-const game = {
+const bot = {
   display: {
     sensors: { level: 1 },
     digTools: { level: 1 },
@@ -17,24 +15,62 @@ const game = {
       maxLoad: 100,
     },
   },
+  currLoc: {
+    x: 0,
+    y: 0,
+  },
+  baseLoc: {
+    x: 0,
+    y: 0,
+  },
+  displayStats: function () {
+    for (let stat in this.display) {
+      for (let data in this.display[stat]) {
+        document.querySelector(`.${stat} .${data}`).textContent = this.display[
+          stat
+        ][data];
+      }
+    }
+  },
+  findBase: function (map) {
+    map.forEach((row, y) => {
+      if (row.includes("base")) {
+        this.baseLoc.y = y;
+        this.baseLoc.x = row.indexOf("base");
+      }
+    });
+  },
+  move: function (destination = [this.baseLoc.x, this.baseLoc.y]) {
+    let prevLoc = document.querySelector(".bot");
+    if (prevLoc) prevLoc.parentNode.removeChild(prevLoc);
+    let loc = boardEl.rows[destination[1]].cells[destination[0]];
+    loc.insertAdjacentHTML("beforeend", `<div class='bot'><div>`);
+  },
 };
 
-function createGrid(size) {
-  let grid = [];
+function createGrid(grid, size) {
   for (let i = 0; i < size; i++) {
     grid.push([]);
     for (let j = 0; j < size; j++) {
-      //randomize the 'land' here
-      grid[i][j] = "land";
+      grid[i][j] = "";
+    }
+  }
+  return grid;
+}
+
+function populateBoard(grid) {
+  for (let row in grid) {
+    for (let cell in grid[row]) {
+      grid[row][cell] = "land";
     }
   }
   //set up base
-  const baseCoords = setupBase(boardSize);
+  const baseCoords = setBaseLoc(grid.length);
   grid[baseCoords[1]][baseCoords[0]] = "base";
   return grid;
 }
 
-function setupBase(size) {
+function setBaseLoc(size) {
   const x = Math.floor(Math.random() * size);
   let y;
   if (x === 0 || x === size - 1) {
@@ -53,20 +89,25 @@ function displayGrid(grid) {
     row.forEach((cell) => {
       let newCellEl = document.createElement("td");
       newCellEl.classList.add(cell);
-      boardEl.insertAdjacentElement("beforeend", newCellEl);
+      newRowEl.insertAdjacentElement("beforeend", newCellEl);
     });
   });
 }
 
-function displayGameStats(game) {
-  for (let stat in game) {
-    for (let deet in game[stat]) {
-      document.querySelector(`.${stat} .${deet}`).textContent =
-        game[stat][deet];
-    }
-  }
+boardEl.addEventListener("click", (e) => {
+  bot.move([e.target.cellIndex, e.target.parentNode.rowIndex]);
+});
+
+function gameInit() {
+  const boardSize = 8;
+  const gameStatus = [];
+
+  createGrid(gameStatus, boardSize);
+  populateBoard(gameStatus);
+  displayGrid(gameStatus);
+
+  bot.displayStats();
+  bot.findBase(gameStatus);
+  bot.move();
 }
-
-displayGrid(createGrid(boardSize));
-
-displayGameStats(game.display);
+gameInit();
